e411253f2578f02efb928e882764bc8c2397a82b
Saran <saranraj22222@gmail.com>
2025-12-14 11:47:58 +0530

    Created Fast API main file

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
-def main():
-    print("Hello from ai-trip-planner!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from agent.agentic_workflow import GraphBuilder
+from utils.save_to_document import save_document
+from starlette.responses import JSONResponse
+import os
+import datetime
+from dotenv import load_dotenv
+from pydantic import BaseModel
 
+load_dotenv()
+app = FastAPI(
+    title="AI Trip Planner",
+    description="An AI-powered trip planner and expense estimator using agentic workflows.",
+)
 
-if __name__ == "__main__":
-    main()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class QueryRequest(BaseModel):
+    question: str
+
+@app.post("/query")
+async def query_travel_agent(query:QueryRequest):
+    try:
+        print(query)
+        graph = GraphBuilder(model_provider="groq")
+        react_app = graph()
+        png_graph = react_app.get_graph().draw_mermaid_png()
+        with open("my_graph.png","wb") as f:
+            f.write(png_graph)
+        print(f"Graph saved as 'my_graph.png' in {os.getcwd()}")
+
+        messages = {"messages":[query.question]}
+        output = react_app.invoke(messages)
+
+        if isinstance(output,dict) and "messages" in output:
+            final_op = output["messages"][-1].content
+        else:
+            final_op = str(output)
+        return {"answer": final_op}
+    
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
